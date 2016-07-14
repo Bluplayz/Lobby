8bc93787e979b8b7a1ad4c1c8c11f310d9122aa6
Erkam Kahraman <erkamkahriman@gmail.com>
2016-07-14 12:34:05 +0200

    Update Main.php

--- src/Lobby/Main.php
+++ src/Lobby/Main.php
@@ -5,6 +5,7 @@ namespace Lobby;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\Player;
 use pocketmine\utils\TextFormat as C;
 
 class Main extends PluginBase {
@@ -13,5 +14,18 @@ class Main extends PluginBase {
     $this->getLogger()->info(C::GREEN."Aktiviert!");
   }
   public function onDisable(){
-    $this->getLogger()->info(C::RED."Deaktiviert!")
+    $this->getLogger()->info(C::RED."Deaktiviert!");
   }
+  public function onCommand(CommandSender $s, Command $cmd,$label, array $args){
+    $defaultspawn = $this->getServer()->getDefaultLevel()->getSpawnLocation();
+    switch($cmd->getName()){
+      case "lobby":{
+        if($sender instanceof Player){
+           $sender->getPlayer()->teleport($defaultspawn);
+        }else{
+          $sender->sendMessage(TextFormat::RED . "Use this command in-game!");
+        }
+      }
+    }
+  }
+}
