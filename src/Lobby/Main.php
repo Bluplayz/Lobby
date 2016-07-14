7c908df7bce986bdf0aea58e11ed9672833cda73
Erkam Kahraman <erkamkahriman@gmail.com>
2016-07-14 12:19:57 +0200

    Update Main.php

--- src/Lobby/Main.php
+++ src/Lobby/Main.php
@@ -3,3 +3,11 @@
 namespace Lobby;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as C;
+
+class Main extends PluginBase {
+  
+  public function onEnable(){
+    $this->getLogger()->info(C::GREEN."Aktiviert!")
